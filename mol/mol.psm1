7e6f8c1e125b2976de294d5f08138465d26a4d2f
master <justinbraun335@gmail.com>
2026-09-13 18:39:02 -0400

    chore: update mol

--- mol/mol.psm1
+++ mol/mol.psm1
@@ -5,31 +5,42 @@
 # =============================== GET-MACHINEINFO ================================
 
 function Get-MachineInfo {
+    [CmdletBinding()]
     param(
+        [Parameter(ValueFromPipeline = $true)]
         [string[]]$ComputerName,
+
         [string]$LogFailuresToPath,
         [string]$Protocol = 'WSMAN',
         [switch]$ProtocolFallback
     )
+    
+    BEGIN {
+    }
 
-    foreach ($Computer in $ComputerName) {
-        # Establish session protocol
-        if ($Protocol -eq 'DCOM') {
-            $Option = New-CimSessionOption -Protocol DCOM
-        } else {
-            $Option = New-CimSessionOption -Protocol WSMAN
+    PROCESS {
+        foreach ($Computer in $ComputerName) {
+            # Establish session protocol
+            if ($Protocol -eq 'DCOM') {
+                $Option = New-CimSessionOption -Protocol DCOM
+            } else {
+                $Option = New-CimSessionOption -Protocol WSMAN
+            }
+
+            # Connect session
+            $Session = New-CimSession -ComputerName $Computer -SessionOption $Option
+
+            # Query data
+            $Os = Get-CimInstance -ClassName Win32_OperatingSystem -CimSession $Session
+
+            # Close session
+            $Session | Remove-CimSession
+
+            $Os | Select-Object -Property @{Name = 'ComputerName'; Expression = {$Computer}}, Version, ServicePackMajorVersion
         }
+    }
 
-        # Connect session
-        $Session = New-CimSession -ComputerName $Computer -SessionOption $Option
-
-        # Query data
-        $Os = Get-CimInstance -ClassName Win32_OperatingSystem -CimSession $Session
-
-        # Close session
-        $Session | Remove-CimSession
-
-        $Os | Select-Object -Property @{Name = 'ComputerName'; Expression = {$Computer}}, Version, ServicePackMajorVersion
+    END {
     }
 }
 
