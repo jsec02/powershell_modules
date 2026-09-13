e9f8632cb80349c92f76773c409c59898fdc0b12
master <justinbraun335@gmail.com>
2026-09-13 15:52:16 -0400

    chore: import Set-MasterServiceLogon

--- mol/mol.psm1
+++ mol/mol.psm1
@@ -1,3 +1,9 @@
+# ================================================================================
+# =                                     MOL                                      =
+# ================================================================================
+
+# =============================== GET-MACHINEINFO ================================
+
 function Get-MachineInfo {
     param(
         [string[]]$ComputerName,
@@ -25,4 +31,48 @@ function Get-MachineInfo {
 
         $Os | Select-Object -Property @{Name = 'ComputerName'; Expression = {$Computer}}, Version, ServicePackMajorVersion
     }
+}
+
+# ============================ SET-MASTERSERVICELOGON ============================
+
+function Set-MasterServiceLogon {
+    param(
+        [string]$ServiceName,
+        [string[]]$ComputerName,
+        [string]$NewPassword,
+        [string]$NewUser,
+        [string]$ErrorLogFilePath
+    )
+
+    if ($PSBoundParameters.ContainsKey('NewUser')) {
+        $Arguments = @{
+            StartName = $NewUser
+            StartPassword = $NewPassword
+        }
+    } else {
+        $Arguments = @{
+            StartPassword = $NewPassword
+        }
+    }
+
+    foreach ($Computer in $ComputerName) {
+        $SessionOption = New-CimSessionOption -Protocol Wsman
+        $Session = New-CimSession -SessionOption $SessionOption -ComputerName $Computer
+
+        $Method = @{
+            Query = "SELECT * FROM Win32_Service WHERE name = '$ServiceName'"
+            MethodName = 'Change'
+            Arguments = $Arguments
+            ComputerName = $Computer
+        }
+
+        Invoke-CimMethod @Method | ForEach-Object {
+            [PSCustomObject]@{
+                ComputerName = $Computer
+                Result = $_.ReturnValue
+            }
+        }
+
+        $Session | Remove-CimSession
+    } 
 }
