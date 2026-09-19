45cbde668fe8cea1d2f43018eaccfe1942a9f182
master <justinbraun335@gmail.com>
2026-09-18 22:46:57 -0400

    chore: regular update

--- mol/mol.psm1
+++ mol/mol.psm1
@@ -5,6 +5,41 @@
 # =============================== GET-MACHINEINFO ================================
 
 function Get-MachineInfo {
+    <#
+    .SYNOPSIS
+    Retrieves specific information about one or more computers using WMI or CIM.
+
+    .DESCRIPTION
+    This command uses either WMI or CIM to retrieve specific information about
+    one or more computers. You must run this command as a user with
+    permission to query CIM or WMI on the machines involved remotely. You can
+    specify a starting protocol (CIM by default), and specify that, in the
+    event of a failure, the other protocol be used on a per-machine basis.
+
+    .PARAMETER ComputerName
+    One or more computer names. When using WMI, this can also be IP addresses.
+    IP addresses may not work for CIM.
+
+    .PARAMETER LogFailuresToPath
+    A path and filename to write failed computer names to. If omitted, no log
+    will be written.
+
+    .PARAMETER Protocol
+    Valid values: Wsman (uses CIM) or Dcom (uses WMI). It will be used for all
+    machines. "Wsman" is the default.
+
+    .PARAMETER ProtocolFallback
+    Specify this to try the other protocol if a machine fails automatically.
+
+    .EXAMPLE
+    Get-MachineInfo -ComputerName ONE,TWO,THREE
+    This example will query three machines.
+
+    .EXAMPLE
+    Get-ADUser -filter * | Select -Expand Name | Get-MachineInfo
+    This example will attempt to query all machines in AD.
+    #>
+
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true,
@@ -92,6 +127,36 @@ function Get-MachineInfo {
 # ============================ SET-MASTERSERVICELOGON ============================
 
 function Set-MasterServiceLogon {
+    <#
+    .SYNOPSIS
+    Sets service login name and password.
+
+    .DESCRIPTION
+    This command uses either CIM (default) or WMI to set the service
+    password, and optionally the logon user name, for a service,
+    which can be running on one or more remote machines. You must
+    run this command as a user who has permission to perform this task,
+    remotely, on the computers involved.
+
+    .PARAMETER ServiceName
+    The name of the service. Query the Win32_Service class to verify
+    that you know the correct name.
+
+    .PARAMETER ComputerName
+    One or more computer names. Using IP addresses will fail with CIM;
+    they will work with WMI. CIM is always attempted first.
+
+    .PARAMETER NewPassword
+    A plain-text string of the new password.
+
+    .PARAMETER NewUser
+    Optional; the new logon user name, in DOMAIN\USER format.
+
+    .PARAMETER ErrorLogFilePath
+    If provided, this is a path and filename of a text file where failed
+    computer names will be logged.
+    #>
+
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true,
